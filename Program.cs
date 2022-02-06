456cee768f0f93a0a5332516fbdaf1220e86fcce
Semenov Nikita <semenov.nikita.95@mail.ru>
2022-02-06 15:12:41 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2:** Даны два числа. Показать большее и меньшее число
+
+/* void method(int x, int y)
+{
+    if (x>y)
+    {
+        Console.WriteLine($"Наибольшее число Х={x}");
+        Console.WriteLine($"Наименьшее число Y={y}");
+    }
+    if (y>x)
+    {
+        Console.WriteLine($"Наибольшее число Y={y}");
+        Console.WriteLine($"Наименьшее число Х={x}");
+    }
+
+}
+method(3,4); */
+
+//Задача 4:** Найти максимальное из трёх чисел.
+
+/* void method(int x, int y, int z)
+{
+    if (x>y&&x>z)
+    {
+        Console.WriteLine($"Наибольшее число Х={x}");
+    }
+    if (y>x&&y>z)
+    {
+        Console.WriteLine($"Наибольшее число Y={y}");
+    }
+    if (z>x&&z>y)
+    {
+        Console.WriteLine($"Наибольшее число Z={z}");
+    }
+
+} 
+method(3, 7, 9); */
+
+
+//Задача 6:** Выяснить является ли число чётным
+
+/* void method(int x)
+{
+    if (x%2==0)
+    {
+        Console.WriteLine($"Число четное");
+    }
+    else
+    Console.WriteLine("Число нечетное");
+    
+} 
+method(4); */
+
+//Задача 8:** Показать чётные числа от 1 до N. 
+
+/* void method(int x)
+{
+    for (int i = 0; i < x; i++)
+    {
+        if (i%2==0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+method(20); */
+

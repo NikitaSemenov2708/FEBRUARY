97416597ee76a32382c9352d6850fc683cbfde37
Semenov Nikita <semenov.nikita.95@mail.ru>
2022-02-15 22:05:54 +0300

    11

---
+++ home_work_4/Program.cs
@@ -0,0 +1,68 @@
+﻿//Задача 26: Возведите число А в натуральную степень B, используя цикл.
+
+/* double met (double a, double b)
+{
+    double res=0;
+    while (a!=0)
+    {
+        res= Math.Pow(a,b);
+        break;
+    }
+    return res;
+    
+}
+double quadro = met(4, 2);
+Console.WriteLine(quadro); */
+//Задача 28: Подсчитайте сумму цифр в числе.
+
+/* int method (int number)
+{
+    
+    int res=0;
+    while (number%10>0)
+    {
+        res=res+number%10;
+        number=number/10;
+    }
+    return res;
+}
+int num = method(5254);
+Console.WriteLine(num); */
+//Задача 30: Покажите кубы чисел, заканчивающихся на чётную цифру.
+/* int n= int.Parse(Console.ReadLine());
+int[] arr = Enumerable.Range(1,n).ToArray();
+
+    foreach (int item in arr)
+{
+    double Pow = Math.Pow(item,3);
+    if (Pow%2==0)
+    {
+        Console.WriteLine(Pow);
+    }
+} */
+
+
+
+//Задача 32: Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран 
+
+int getRandom ()
+{
+
+    return new Random().Next(0, 2);
+}
+
+int[] N= new int[8];
+int index=0;
+while (index<8)
+{
+     N[index]=getRandom();
+     index++;
+}
+
+index=0;
+
+while (index<8)
+{
+Console.Write(N[index]);
+index++;
+}

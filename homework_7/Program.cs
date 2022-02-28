985fd90f1353d230f95e9c03cc9a0bf1b41a3106
Semenov Nikita <semenov.nikita.95@mail.ru>
2022-02-28 21:36:21 +0300

    15

---
+++ homework_7/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+//Задача 49: Показать двумерный массив размером m×n, заполненный вещественными числами.
+ /*  void PrintArray(double[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i , j]}; ");
+    }
+   Console.WriteLine(); 
+}
+}
+void FillArray(double[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+      for (int j = 0; j < matr.GetLength(1); j++)
+      {
+          matr[i,j] = new Random().Next(1, 10)+ new Random().NextDouble() ;
+      }
+      
+  }
+
+}
+double[,] matrix = new double [3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix); */
+
+//Задача 51: Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+/* void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i , j]}; ");
+    }
+   Console.WriteLine(); 
+}
+}
+void FillArray(int[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+      for (int j = 0; j < matr.GetLength(1); j++)
+      {
+          matr[i,j] = i+j ;
+      }
+      
+  }
+
+}
+int[,] matrix = new int [4, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix); */
+
+//Задача 53: В двумерном массиве показать позиции числа, заданного пользователем, или указать, что такого элемента нет.
+
+/* void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i , j]}; ");
+    }
+   Console.WriteLine(); 
+}
+}
+void FillArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+  {
+      for (int j = 0; j < matr.GetLength(1); j++)
+      {
+          matr[i,j] = new Random().Next(1, 10) ;
+      }
+      
+  }
+
+}
+
+void ChangeArray(int[,] matr)
+{
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+string result=string.Empty;
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+      for (int j = 0; j < matr.GetLength(1); j++)
+      {
+          matr[i,j] = new Random().Next(1, 10);
+          if (matr[i,j]==number)
+          {
+              result=result+$"{matr[i,j]}; ";
+              Console.WriteLine(result);
+          }
+          
+      }
+      
+  }
+if (result==string.Empty)
+          {
+              Console.WriteLine("Такого числа нет");
+          }
+}
+int[,] matrix = new int [4, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine();
+ChangeArray(matrix);
+PrintArray(matrix); */
+
+//Задача 55: Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов
---
+++ praktika_9/Program.cs
@@ -0,0 +1,48 @@
+﻿//Показать натуральные числа от 1 до N, N задано
+
+/* string NumberLook(int n)
+{
+    string result= string.Empty;
+    if(n==1) return result+n+" ";
+    else return NumberLook(n-1)+n+" " ;
+    
+}
+Console.WriteLine(NumberLook(10)); */
+//Показать натуральные числа от M до N, N и M заданы
+
+/* string NumberLook(int M, int N)
+{
+    //string result= string.Empty;
+    if(M==N) return N.ToString();
+    else return M+" "+NumberLook(M+1,N);
+}
+Console.WriteLine(NumberLook(10,20));  */
+
+//Найти сумму цифр числа
+
+ /* int number(int A)
+{
+
+    if(A<10) return A;
+    else return A%10+(number(A/10));
+}
+Console.WriteLine(number(525));  */
+
+//Написать программу возведения числа А в целую стень B
+
+ /* int method (int A, int B)
+{
+    if(B==0) return 1;
+    else return A*method(A,B-1);
+
+}
+Console.WriteLine(method(2,5)); */ 
+
+//Показать натуральные числа от 1 до N, N задано
+
+ /* string NumberLook(int n)
+{
+    return n == 1 ? n.ToString() : NumberLook(n-1)+" "+n;
+    
+}
+Console.WriteLine(NumberLook(10)); */

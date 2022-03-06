47d59683092e5ccc2b898ddefb97453b13b3aadf
Semenov Nikita <semenov.nikita.95@mail.ru>
2022-03-07 00:40:31 +0300

    21

--- praktika_10/Program.cs
+++ praktika_10/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine(Fibonacci(5));  */
 /* Console.WriteLine("Введите буквы из которых хотите собрать слово");
 string words = Console.ReadLine().Split(" ").ToString();
 string temp = words; */
-int n =1;
+/* int n =1;
 void FineWords (string alphabet, char[] word, int length=0  )
 {
     if(length==word.Length)
@@ -24,5 +24,30 @@ void FineWords (string alphabet, char[] word, int length=0  )
     }
 
 }
-FineWords("aисв", new char[2]);
+FineWords("aисв", new char[2]); */
 
+// в указанном диапазоне вывести все взаимно простые числа указанному число от А до В и вводим число N// без N
+
+string NumberLook(int M, int N)
+{   
+    
+
+    for (int i = M; i < N; i++)
+    {
+       if (i%2!=0 &&i%3!=0&&i%5!=0&&i%7!=0)
+    {
+    if(i==N) return N.ToString();
+    else return i+" "+NumberLook(i+1,N);
+    }
+    if ((i/2<2&&i/3<=1||i%5==0||i%7==0)&&i<10)
+    {
+        return i+" "+NumberLook(i+1,N);
+    } 
+    }
+
+    return " ";
+    
+    
+    
+}
+Console.WriteLine(NumberLook(1,50));
